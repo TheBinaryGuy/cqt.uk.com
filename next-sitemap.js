60c805f79a93f305c90999e80e27193bc56c7282
TheBinaryGuy <hello@thebinary.dev>
2022-09-12 23:05:06 +0530

    :pencil2: Changed www to non-www.

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl:
-        `https://${process.env.NEXT_PUBLIC_DOMAIN}` || 'https://www.cqt.uk.com',
+        `https://${process.env.NEXT_PUBLIC_DOMAIN}` || 'https://cqt.uk.com',
     generateRobotsTxt: true,
 };
