7767a71f336b62721a2a09c993fc19fbaccd8ab4
TheBinaryGuy <hello@thebinary.dev>
2023-03-21 12:10:03 +0530

    reverted resources page to ssr

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -3,39 +3,30 @@ import Resources from '@/components/Resource';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { Resource } from '@/schemas/sanity-types';
 import groq from 'groq';
-import {
-    GetServerSideProps,
-    GetStaticProps,
-    InferGetServerSidePropsType,
-    InferGetStaticPropsType,
-} from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getServerSession } from 'next-auth';
-import { signIn, useSession } from 'next-auth/react';
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 
-const ResourcesPage: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
-    resources,
-}) => {
-    const { status } = useSession();
-
-    useEffect(() => {
-        if (status === 'unauthenticated') {
-            signIn();
-        }
-    }, [status]);
-
-    if (status === 'unauthenticated' || status === 'loading') {
-        return null;
-    }
-
-    return <Resources resources={resources} />;
-};
+const ResourcesPage: FC<
+    InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ resources }) => <Resources resources={resources} />;
 
 export default ResourcesPage;
 
-export const getStaticProps: GetStaticProps<{
+export const getServerSideProps: GetServerSideProps<{
     resources: Resource[];
-}> = async () => {
+}> = async ctx => {
+    const session = await getServerSession(ctx.req, ctx.res, authOptions);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: `/api/auth/signin?callbackUrl=${ctx.req.url}`,
+            },
+            props: { resources: [] },
+        };
+    }
+
     const resources =
         (await client.fetch<Resource[] | null>(
             groq`*[_type == "resource"] {
@@ -59,9 +50,6 @@ export const getStaticProps: GetStaticProps<{
         )) ?? [];
 
     return {
-        props: {
-            resources,
-        },
-        revalidate: 10800,
+        props: { resources },
     };
 };
